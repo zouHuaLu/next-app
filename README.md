a062b192d8a59690f7f72a576bd0b93ecc02eb76
zouHuaLu <huangjie1234huang@qq.com>
2023-11-27 14:21:41 +0800

    nextjs更新

---
+++ README.md
@@ -0,0 +1,14 @@
+如果学过react的话会很容易上手
+主要区别就是NEXT.js提供了三个方法用于服务端渲染
+
+1.  getInitialProps （最新版本不用它了）
+2.  getStaticProps
+3.  getStaticPaths
+4.  getServerSideProps
+
+学会这些方法的使用，其余的都是react的知识点了
+
+推荐看这篇文章：[快速理解 Next.js 中数据获取方式的作用及区别（getInitialProps、getServerSideProps、getStaticProps）](https://www.pipipi.net/20406.html)
+
+
+不理解渲染方式的可以看这篇：[快速理解 Next.js 中各种渲染方式的作用及区别（SSG、SSR、ISR、CSR）](https://juejin.cn/post/7213653429415559223)
--- pages/Blog.js
+++ pages/Blog.js
@@ -1,10 +1,16 @@
+import {useState} from "react";
+import styles from './Blog.module.css'
 export default function Blog({res}) {
-    const {data,name,age} = res
+    const [contentData, setContentData] = useState(1)
+    const {data, name, age} = res
     return (
         <div>
             <div>日期：{data}</div>
             <div>姓名：{name}</div>
             <div>年龄：{age}</div>
+            <div className={styles.btn} onClick={() => {
+                setContentData(contentData + 1)
+            }}>数据：{contentData}</div>
         </div>
     )
 }
